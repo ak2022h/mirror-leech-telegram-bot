65662977c72ce6b845fce1620d15dddefea41339
ak2022h <100077675+ak2022h@users.noreply.github.com>
2022-03-29 20:54:28 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,8 +64,8 @@ def start(update, context):
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-//This bot can mirror all your links to Google Drive!
-//Type /{BotCommands.HelpCommand} to get a list of available commands
+This bot can mirror all your links to Google Drive!
+Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
